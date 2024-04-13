4fe86e13068fabda53adcbaf4e00e0c3fdccb868
DESKTOP-LKCI9L4 <emily.pn@sisma.org.il>
2024-04-13 21:20:45 +0300

    finish

--- app/src/main/java/com/example/emilypanfilovequation/MainActivity.java
+++ app/src/main/java/com/example/emilypanfilovequation/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.emilypanfilovequation;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -17,11 +21,12 @@ import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
     private EditText etA,etB,etC;
-    private Button random,solve;
+    private Button random,solve,delete;
     Random rnd=new Random();
     String strA,strB,strC;
     float numA,numB,numC;
     private final int REQUEST_CODE=100;
+    private TextView tv,name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +43,33 @@ public class MainActivity extends AppCompatActivity {
         etC=(EditText) findViewById(R.id.etC);
         random=(Button) findViewById(R.id.random);
         solve=(Button) findViewById(R.id.solve);
+        delete=(Button) findViewById(R.id.delete);
+        tv=findViewById(R.id.tv);
+        name=findViewById(R.id.name);
         random.setBackgroundColor(Color.rgb(0,255,250));
         solve.setBackgroundColor(Color.rgb(0,255,250));
+        delete.setBackgroundColor(Color.rgb(0,255,250));
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode==REQUEST_CODE){
+            if(resultCode== Activity.RESULT_OK){
+                if(data!=null){
+                    String str1=data.getStringExtra("answer1");
+                    String str2=data.getStringExtra("answer2");
+                    tv.setText("X1="+str1+"X2="+str2);
+
+                }
+                else {
+                    Log.i("MainActivity","NO data returned");
+                }
+            }
+            else {
+                Log.i("MainActivity","Bad return code");
+            }
+        }
     }
 
     public void random(View view) {
@@ -62,6 +92,18 @@ public class MainActivity extends AppCompatActivity {
             si.putExtra("c",numC);
             startActivityForResult(si,REQUEST_CODE);
         }
+
+    }
+
+
+    public void delete(View view) {
+        etA.setText("");
+        etA.getHint();
+        etB.setText("");
+        etB.getHint();
+        etC.setText("");
+        etC.getHint();
+        tv.setText("");
 
     }
 }
--- app/src/main/java/com/example/emilypanfilovequation/MainActivity2.java
+++ app/src/main/java/com/example/emilypanfilovequation/MainActivity2.java
@@ -1,5 +1,7 @@
 package com.example.emilypanfilovequation;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -17,6 +19,10 @@ public class MainActivity2 extends AppCompatActivity {
     private Button back;
     private ImageView parabola;
     private TextView answer;
+    private float a,b,c,delta;
+    private String solution1,solution2;
+    Intent gi=getIntent();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,9 +38,48 @@ public class MainActivity2 extends AppCompatActivity {
         parabola=(ImageView) findViewById(R.id.parabola);
         answer=findViewById(R.id.answer);
         back.setBackgroundColor(Color.rgb(0,255,250));
+        a=gi.getFloatExtra("a",0);
+        b=gi.getFloatExtra("b",0);
+        c=gi.getFloatExtra("c",0);
+        delta=b*b-4*a*c;
+        if (delta==0 && a>0){
+            parabola.setImageResource(R.drawable.parabola1);
+        }
+        if (delta>0 && a>0){
+            parabola.setImageResource(R.drawable.parabola2);
+        }
+        if (delta<0 && a>0){
+            parabola.setImageResource(R.drawable.parabola3);
+        }
+        if (delta==0 && a<0){
+            parabola.setImageResource(R.drawable.parabola4);
+        }
+        if (delta>0 && a<0){
+            parabola.setImageResource(R.drawable.parabola5);
+        }
+        if (delta<0 && a<0){
+            parabola.setImageResource(R.drawable.parabola6);
+        }
+        if (delta==0){
+            solution1=(Float.valueOf(-b)+Math.sqrt(delta))/(2*a)+"";
+            solution2="no solution";
+        }
+        if (delta>0){
+            solution1=(Float.valueOf(-b)+Math.sqrt(delta))/(2*a)+"";
+            solution2=(Float.valueOf(-b)-Math.sqrt(delta))/(2*a)+"";
+        }
+        if (delta<0){
+            solution1="no solution";
+            solution2="no solution";
+        }
+        answer.setText("X1="+solution1+" , "+"X2="+solution2);
+
     }
 
     public void back(View view) {
+        gi.putExtra("answer1",solution1);
+        gi.putExtra("answer2",solution2);
+        setResult(Activity.RESULT_OK,gi);
         finish();
     }
 }
